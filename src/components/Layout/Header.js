7bc622fa47b8cb2c9649085dd1e56ce831d7d4c5
Cameron Partee <cp3082@nyu.edu>
2021-09-12 17:53:23 -0400

    Added headercartbutton and correlating code

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-export const Header = () => {
+export const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <h1>Your Universal Cart</h1>
-        <div className={classes["icon-row"]}>
+        <h1>YUC</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+        {/* <div className={classes["icon-row"]}>
           <i className="fa fa-shopping-bag"></i>
           <i className="fa fa-heart"></i>
-        </div>
+        </div> */}
       </header>
     </>
   );
