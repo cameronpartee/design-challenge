7041b8056b2d70289e2064d6fc0e1f562d4cb0e4
cameronpartee <cameronepartee@gmail.com>
2021-10-09 19:51:16 -0400

    add Selection and Board components

---
+++ src/components/Items/Selection.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Selection = () => {
+  return <Container></Container>;
+};
+
+export default Selection;
+
+const Container = styled.div`
+  width: 300px;
+  height: 400px;
+  background: rgb(202, 202, 202);
+  border: 3px dashed rgb(192, 192, 192);
+  margin: auto;
+`;
