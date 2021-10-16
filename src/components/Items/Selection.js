0f91e9387347f18e1c16fd52d7555050efe73531
cameronpartee <cameronepartee@gmail.com>
2021-10-16 14:34:24 -0400

    add the new indication to the selection

--- src/components/Items/Selection.js
+++ src/components/Items/Selection.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const Selection = (props) => {
-  return <Container>{props.item.name}</Container>;
+  const isNew = props.item.tags.includes("new");
+  return <Container>{isNew && <New>New</New>}</Container>;
 };
 
 export default Selection;
@@ -12,4 +13,17 @@ const Container = styled.div`
   background: rgb(202, 202, 202);
   border: 3px dashed rgb(192, 192, 192);
   margin: auto;
+`;
+
+const New = styled.div`
+  margin-top: 8px;
+  margin-left: -2px;
+  width: 80px;
+  height: 35px;
+  background: rgb(0, 190, 0);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.1rem;
 `;
