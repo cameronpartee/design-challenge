238739669b7b1d0aa5b0269836888ce8ff3512cf
Cameron Partee <cp3082@nyu.edu>
2021-10-17 23:02:31 -0400

    add selection and corresponding components

--- src/components/Items/Selection.js
+++ src/components/Items/Selection.js
@@ -1,29 +1,20 @@
 import styled from "styled-components";
+import SelectionImage from "./SelectionImage";
+import SelectionInfo from "./SelectionInfo";
 
 const Selection = (props) => {
-  const isNew = props.item.tags.includes("new");
-  return <Container>{isNew && <New>New</New>}</Container>;
+  return (
+    <Container>
+      <SelectionImage item={props.item} />
+      <SelectionInfo item={props.item} />
+    </Container>
+  );
 };
 
 export default Selection;
 
 const Container = styled.div`
+  height: 350px;
   width: 300px;
-  height: 400px;
-  background: rgb(202, 202, 202);
-  border: 3px dashed rgb(192, 192, 192);
   margin: auto;
-`;
-
-const New = styled.div`
-  margin-top: 8px;
-  margin-left: -2px;
-  width: 80px;
-  height: 35px;
-  background: rgb(0, 190, 0);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  font-size: 1.1rem;
 `;
