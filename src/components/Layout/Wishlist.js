3009fa5dfe517c9d40bcb0fa8bcddb2353bf9121
cameronpartee <cameronepartee@gmail.com>
2021-10-17 23:02:50 -0400

    update grid styles

--- src/components/Layout/Wishlist.js
+++ src/components/Layout/Wishlist.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Board from "./Board";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar/Sidebar";
 
 const Wishlist = () => {
   return (
@@ -20,7 +20,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
-  grid-template-rows: 100px auto;
+  grid-template-rows: 50px auto;
 `;
 
 const Header = styled.div`
