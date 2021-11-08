17bdc943d09e2e5156a994c46a82d55d8e8bb7c9
cameronpartee <cameronepartee@gmail.com>
2021-11-07 21:32:24 -0500

    comment out v1 and use v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Header from "./components/Layout/Header";
-import Container from "./components/Layout/Container";
-import Cart from "./components/Cart/Cart";
-import { useState } from "react";
-import CartProvider from "./store/CartProvider";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Wishlist from "./components/Layout/Wishlist";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
+  return <Wishlist />;
+}
+
+export default App;
+
+/*
+
+// import Header from "./components/Layout/V1/Header";
+// import Container from "./components/Layout/V1/Container";
+// import Cart from "./components/Cart/Cart";
+// import { useState } from "react";
+// import CartProvider from "./store/CartProvider";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+ const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -16,25 +26,39 @@ function App() {
     setCartIsShown(false);
   };
 
-  return (
-    <Router>
-      <CartProvider>
-        {cartIsShown && <Cart onClose={hideCartHandler} />}
-        <Header onShowCart={showCartHandler} />
-        <Switch>
-          <Route path="/want">
-            <Container purchased={false} />
-          </Route>
-          <Route path="/have">
-            <Container purchased={true} />
-          </Route>
-          <Route path="/">
-            <Container purchased={"all"} />
-          </Route>
-        </Switch>
-      </CartProvider>
-    </Router>
-  );
-}
+  const DUMMY_ITEMS_COPY = [
+    {
+      id: "10",
+      name: "SHEIN Snakeskin Print Split Hem Jeans",
+      price: "11.00",
+      size: "M",
+      purchased: "false",
+      img: "https://img.ltwebstatic.com/images3_pi/2021/06/23/16244197303daab2266e8e1c2820c2e16737c50878_thumbnail_405x552.webp",
+      website:
+        "https://us.shein.com/SHEIN-Snakeskin-Print-Split-Hem-Jeans-p-2390970-cat-1934.html",
+      brand: "Shein",
+    },
+  ];
 
-export default App;
+  
+    // <Router>
+    //   <CartProvider>
+    //     {cartIsShown && <Cart onClose={hideCartHandler} />}
+    //     <Header onShowCart={showCartHandler} />
+    //     <Switch>
+    //       <Route path="/details">
+    //         <Details item={DUMMY_ITEMS_COPY[0]} />
+    //       </Route>
+    //       <Route path="/want">
+    //         <Container purchased={false} />
+    //       </Route>
+    //       <Route path="/have">
+    //         <Container purchased={true} />
+    //       </Route>
+    //       <Route path="/">
+    //         <Container purchased={"all"} />
+    //       </Route>
+    //     </Switch>
+    //   </CartProvider>
+    // </Router>
+*/
