0eee8087d1b818ea114774b118575a1940890f57
cameronpartee <cameronepartee@gmail.com>
2022-04-27 22:39:51 -0400

    add a wishlist comp

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,21 +1,13 @@
 import styled from "styled-components";
 import Board from "../components/Layout/Board";
-import Navbar from "../components/Layout/Navbar/Navbar";
 import { useState } from "react";
 
 const Wishlist = () => {
   const [toggleBoard, flipToggleBoard] = useState(true);
 
-  const tester = (name) => {
-    flipToggleBoard(name === "Universal Cart" ? false : true);
-  };
-
   return (
     <Container>
-      <Navbar test={tester}></Navbar>
-      <Main>
-        <Board toggleBoard={toggleBoard}></Board>
-      </Main>
+      <Board toggleBoard={toggleBoard}></Board>
     </Container>
   );
 };
@@ -26,11 +18,6 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
-  grid-template-columns: 230px auto;
-`;
-
-const Main = styled.div`
-display: grid;
-grid-template-rows auto;
-background: rgb(255, 255, 255);
+  grid-template-rows auto;
+  background: rgb(255, 255, 255);
 `;
