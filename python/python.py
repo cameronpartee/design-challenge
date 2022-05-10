d04e39e305ce422bf786f9f92121cba1e62b2fed
cameronpartee <cameronepartee@gmail.com>
2022-05-09 21:08:43 -0400

    modify the webscraper script

--- python/python.py
+++ python/python.py
@@ -1,25 +1,28 @@
+# [HOWTO] run py script in terminal -> python gameover.py
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#id17
+
 from bs4 import BeautifulSoup
 
+# JasleneCorsetMiniDressinWhite.html
+# Summer Dresses _ Annika Bodycon Dress in White.html
 with open("Summer Dresses _ Annika Bodycon Dress in White.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-# soup = BeautifulSoup("<html>a web page</html>", "html.parser")
+prettySoup = soup.get_text().strip().replace(" ", "");
+print(" ".join(prettySoup.split()));
 
-# print(soup.prettify())
-
-# print(soup.get_text())
-
-for link in soup.find_all("link"):
-    print(link)
+# for link in soup.find_all("link"):
+#     print(link)
 
 # for link in soup.find_all("href"):
 #     print(link.get('href'))
 
 
 
-# What do I want - tags 
-# img url 
-# store name 
-# https://www.lucyinthesky.com/favicon.ico" rel="shortcut icon"/>
-# <link href="https://www.lucyinthesky.com/apple-icon-57x57.png" rel="apple-touch-icon" sizes="57x57"/> 
 
+# Data
+# picture
+# store name 
+# favicon
+# --
+# item name - parse for tags 
