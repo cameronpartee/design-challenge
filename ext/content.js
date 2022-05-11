af4482f4899ef45d017ef020323352ffd7996bb2
cameronpartee <cameronepartee@gmail.com>
2022-05-10 21:57:32 -0400

    add comments and todos

--- ext/content.js
+++ ext/content.js
@@ -1,4 +1,5 @@
 // when you are building extensions you need to be very familiar with the dom
+// TODO: LETS GET MORE FAMILIAR WITH THE DOM
 
 let myImages =
   "https://yt3.ggpht.com/yti/APfAmoF3cbabr_gXui2CEhK87UuJFQg_2gGzH0Mj9g3gaw=s88-c-k-c0x00ffffff-no-rj-mo";
@@ -6,9 +7,13 @@ let myImages =
 const imgs = document.getElementsByTagName("img");
 for (let i = 0; i < imgs.length; i++) {
   imgs[i].addEventListener("click", function () {
-    console.log("clicked!");
+    // img src link
     console.log(imgs[i].src);
+    // website link
     console.log(window.location.href);
+    // fav icon
     console.log(document.querySelector("link[rel*='icon']").href);
   });
 }
+
+// TODO: HOW ARE WE STORING THIS INFORMATION? - WHERE SHOULD WE SEND IT TO?
