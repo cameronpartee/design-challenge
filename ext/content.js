e7176f574a626aa99cef87e0523311be63d3b362
cameronpartee <cameronepartee@gmail.com>
2022-05-15 13:32:51 -0400

    add data points to obj structure

--- ext/content.js
+++ ext/content.js
@@ -1,18 +1,16 @@
 // when you are building extensions you need to be very familiar with the dom
 // TODO: LETS GET MORE FAMILIAR WITH THE DOM
+// AND ALSO BROWSER EXTENSIONS
 
-let myImages =
-  "https://yt3.ggpht.com/yti/APfAmoF3cbabr_gXui2CEhK87UuJFQg_2gGzH0Mj9g3gaw=s88-c-k-c0x00ffffff-no-rj-mo";
+const cartItem = {};
 
 const imgs = document.getElementsByTagName("img");
 for (let i = 0; i < imgs.length; i++) {
   imgs[i].addEventListener("click", function () {
-    // img src link
-    console.log(imgs[i].src);
-    // website link
-    console.log(window.location.href);
-    // fav icon
-    console.log(document.querySelector("link[rel*='icon']").href);
+    cartItem.imgSrc = imgs[i].src;
+    cartItem.websiteHref = window.location.href;
+    cartItem.favicon = document.querySelector("link[rel*='icon']").href;
+    console.log(cartItem);
   });
 }
 
