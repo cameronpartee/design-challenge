f3f40573780549e4b4750bd8fbfe0d5beac25bd0
cameronpartee <cameronepartee@gmail.com>
2022-06-16 23:54:13 -0400

    extract data into server.ts

---
+++ v2/src/data/server.ts
@@ -0,0 +1,142 @@
+import * as interfaces from "./interfaces";
+
+export const itemsArray: interfaces.item[] = [
+  {
+    img: "https://m.media-amazon.com/images/I/71FkFR2J7tL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B09YNGRV46/ref=ppx_yo_dt_b_asin_image_o03_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71YGi+Y-RoL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B09W43CFSG/ref=ppx_yo_dt_b_asin_image_o04_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/51zy5kbQYrL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B08X4H4G7D/ref=ppx_yo_dt_b_asin_image_o03_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61lD+JNjZPL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B09FXV56JW/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/51B2PT+AN8L._AC_UX679_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B09FXV56JW/ref=ppx_yo_dt_b_asin_image_o03_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71iDZS6qJOL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/dp/B093GCVH1T?ref=ppx_yo2ov_dt_b_product_details&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71LU7lgUpdL._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/dp/B09NVT7Z5J?ref=ppx_yo2ov_dt_b_product_details&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61-WUraqt-L._AC_UX679_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/dp/B0976ZDFLJ?ref=ppx_yo2ov_dt_b_product_details&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/91gFtxLmV1L._AC_UY879_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/dp/B07Q12L4RF?ref=ppx_yo2ov_dt_b_product_details&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2022/02/21/16454266650c7f0e9d2b988dde15b43eb34baa3349_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/SHEIN-Frenchy-Heart-Embroidery-Pocket-Patched-Drop-Shoulder-Blouse-p-9886115-cat-1733.html",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2021/05/25/16219382561c2446046de3726c35bac366a54d1de7_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/SHEIN-BAE-Solid-Tank-Bodysuit-p-2707496-cat-1882.html",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2022/04/28/165111826250220221738fa3bd62b25efb6d924ad9_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/Striped-Drop-Shoulder-Two-Tone-Blouse-p-10432734-cat-1733.html?scici=Wishlist~~Wishlist~~1~~Wishlist~~~~",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0293/9277/products/03-03-22Studio2_SN_AC_14-30-05_70_TT1168F_Whitecombo_1773_EH_468x@2x.jpg?v=1646785233",
+    store: interfaces.Store.FashionNova,
+    item: "https://www.fashionnova.com/products/waist-no-time-baroque-blouse-white-combo",
+    favicon: "https://play-lh.googleusercontent.com/PMXV8n0p9iOBZhxCELQIywmfCsXa7hRSSbX_CZQFXRxlSIYn_EWCa8cOQVFjLbfJ0oQ",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2022/03/14/1647241283f3b2d7df1207edc70e2192a3666845fa_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/Striped-Contrast-Collar-Tee-p-10004161-cat-1738.html?scici=Wishlist~~Wishlist~~1~~Wishlist~~~~",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0293/9277/products/03-02-22Studio1_MS_LT_11-43-58_32_FE22C098_Lilac_8256_KL_468x@2x.jpg?v=1646679486",
+    store: interfaces.Store.FashionNova,
+    item: "https://www.fashionnova.com/products/talk-sweet-ruffle-top-lilac",
+    favicon: "https://play-lh.googleusercontent.com/PMXV8n0p9iOBZhxCELQIywmfCsXa7hRSSbX_CZQFXRxlSIYn_EWCa8cOQVFjLbfJ0oQ",
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0293/9277/products/02-18-20_Studio_3_RM_LE_17-26-19_69__PT40012E_Blue_P_0008_JK_468x@2x.jpg?v=1582224527",
+    store: interfaces.Store.FashionNova, // make this an enum
+    item: "https://www.fashionnova.com/products/one-time-thing-peplum-top-blue",
+    favicon: "https://play-lh.googleusercontent.com/PMXV8n0p9iOBZhxCELQIywmfCsXa7hRSSbX_CZQFXRxlSIYn_EWCa8cOQVFjLbfJ0oQ",
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0293/9277/products/05-19-22Studio6_RT14-11-25_54_JOVIE03_Black_0039_SG_468x@2x.jpg?v=1653344635",
+    store: interfaces.Store.FashionNova,
+    item: "https://www.fashionnova.com/products/jocelyn-rhinestone-sandals-black",
+    favicon: "https://play-lh.googleusercontent.com/PMXV8n0p9iOBZhxCELQIywmfCsXa7hRSSbX_CZQFXRxlSIYn_EWCa8cOQVFjLbfJ0oQ",
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0293/9277/products/05-18-22Studio6_RT_15-14-24_62_JACELYN30_Nude_2156_PB_468x@2x.jpg?v=1653344260",
+    store: interfaces.Store.FashionNova,
+    item: "https://www.fashionnova.com/products/keeping-you-company-rhinestone-slides-nude",
+    favicon: "https://play-lh.googleusercontent.com/PMXV8n0p9iOBZhxCELQIywmfCsXa7hRSSbX_CZQFXRxlSIYn_EWCa8cOQVFjLbfJ0oQ",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2021/07/17/1626502872bf7bbf9f60abcabfcfe0c56578e62764_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/2pcs-Simple-Anklet-p-2470916-cat-1908.html",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://img.ltwebstatic.com/images3_pi/2021/05/06/1620270155c35f66270c60ee315098ad76bad535a8_thumbnail_900x.webp",
+    store: interfaces.Store.Shein,
+    item: "https://us.shein.com/Rhinestone-Decor-Anklet-p-2628969-cat-1908.html",
+    favicon: "https://apprecs.org/ios/images/app-icons/256/20/878577184.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61SG03kvoPL._AC_UX679_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B09X2S93TM/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&th=1&psc=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/61adJqvYy1S._AC_UY695_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B094H6HJRM/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&th=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71bDqt8yaLL._AC_UX679_.jpg",
+    store: interfaces.Store.Amazon,
+    item: "https://www.amazon.com/gp/product/B084ZF8LZ6/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&th=1",
+    favicon: "https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-current-Logo-2.jpg",
+  },
+];
