b1baa7cb75aba2ad3e4ebd74ef65b95e6c32811b
cameronpartee <cameronepartee@gmail.com>
2022-07-10 23:27:22 -0400

    add additional interfaces

--- v2/src/data/interfaces.ts
+++ v2/src/data/interfaces.ts
@@ -11,6 +11,19 @@ export enum Store {
     Amazon = "Amazon",
     Shein = "Shein",
     FashionNova = "FashionNova",
+    Dillards = "Dillards",
+    FightClub = "FightClub",
+    Asos = "Asos"
 };
+
+export enum Category {
+    Top = "Top",
+    Bottom = "Bottom",
+    Shoe = "Shoe",
+    Dress = "Dress",
+    Skirt = "Skirt",
+    Short = "Short",
+    Jewelry = "Jewelry"
+}
 
 export type dropdownPlaceholders = "PRICE" | "STORE" | "COLOR" | "CATEGORY" | "SEASON" | "BRAND";
